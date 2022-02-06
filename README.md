12722ce8b3f50488eda0db0bbeba789c811f2fe8
clarkchen84 <sizhe.chen1@gmail.com>
2022-02-06 16:07:09 +0800

    60 基本类型注入

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # spring-core
-spring-core-study-project
+####  基础类型注入
+- 原生类型： boolean,int,long,byte,char,double,float,short
+- 标量类型： Number,Character ,Boolean,Enum,Local,Charset,Currency,Property,UUID
+- 常规类型：Object,String,TimeZone,Calendar,Optional等
+- spring 类型： Resource，InputSource，Formatter等
+
+
+
+# 使用maven
+
+#### 现在源码的命令
+
+mvn dependency:resolve -Dclassifier=source
+
+# git 命令
+
+#### 使用Git 切换切换远程 分支到head
+
+``` git
+git remote set-head $REMOTE_NAME $BRANCH
+```
+
+因此，例如，将远程头部切换`origin`到分支`develop`将是：
+
+```
+git remote set-head origin develop
+```
+
+
+
+
+
+
+
+
+
+
+
--- ioc-container-overview/src/main/java/sizhe/chen/think/in/spring/ioc/overview/dependency/domain/User.java
+++ ioc-container-overview/src/main/java/sizhe/chen/think/in/spring/ioc/overview/dependency/domain/User.java
@@ -1,10 +1,36 @@
 package sizhe.chen.think.in.spring.ioc.overview.dependency.domain;
 
+import org.springframework.core.io.Resource;
+import sizhe.chen.think.in.spring.ioc.overview.dependency.enums.City;
+
 public class User {
 
     private String name;
     private Long id;
     private Integer age;
+    private City city;
+    private Resource configLocation;
+
+    public Resource getConfigLocation() {
+        return configLocation;
+    }
+
+    public void setConfigLocation(Resource configLocation) {
+        this.configLocation = configLocation;
+    }
+
+    public City getCity() {
+        return city;
+    }
+
+    public void setCity(City city) {
+        this.city = city;
+    }
+
+
+    public User() {
+
+    }
 
     public static User createUser(){
         User user = new User();
@@ -44,6 +70,8 @@ public class User {
                 "name='" + name + '\'' +
                 ", id=" + id +
                 ", age=" + age +
+                ", city=" + city +
+                ", configLocation=" + configLocation +
                 '}';
     }
 }
